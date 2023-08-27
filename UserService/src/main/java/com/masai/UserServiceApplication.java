aa9a79ef7e4f7ed3807135b6e138ba6da57636a8
Himanshu04210 <himanshupoly76075@gmail.com>
2023-08-27 22:21:28 +0530

    some new things added

--- UserService/src/main/java/com/masai/UserServiceApplication.java
+++ UserService/src/main/java/com/masai/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+//@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
